d4a76d6114ebb9cd31b91246496a1ff343a47ebf
guillaume schwaederle <guillaume.schwaederle@gmail.com>
2017-11-24 17:52:10 +0100

    Cocktail end

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,4 +2,5 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, dependent: :destroy, through: :doses
   validates :name, uniqueness: true, presence: true
+  mount_uploader :photo, PhotoUploader
 end
